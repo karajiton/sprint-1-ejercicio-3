b44d20bc5bece99ac4f36ca2f5eb68ad31e11ebc
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-06-12 01:00:12 +0200

    Add files via upload

---
+++ Sprint1ejer3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+<title>dodod</title>
+<?php
+$num = 2;
+$double = 9.5;
+$string = "hola soy jonathan";
+$boo = true;
+
+echo $num ," , ", $double , " , ", $string, " , ", $boo; 
+echo " , "; 
+define("yo", "Jonathan");
+echo "mi nombre es " .  yo;
+
+
+?> 
+</head>
+</html>
