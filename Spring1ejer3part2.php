65d872fae7789f0ab71a87475c830a1eca2e164c
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-06-12 11:08:32 +0200

    Add files via upload
    ejercicio 2

---
+++ Spring1ejer3part2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <?php
+        $string = "Hello, World!. ";
+        echo strtoupper($string), " , ";
+        echo strlen($string), " , " ;
+        echo strrev($string), " . ";
+        $nuevo = "Este es el curso de PHP";
+        echo $string . $nuevo;
+        ?>
+    </head>
+</html>
