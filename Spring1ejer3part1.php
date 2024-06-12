19457f012e01d419af4b237c230a4f8cf4d5e151
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-06-12 11:06:19 +0200

    Add files via upload
    ejercicio1

---
+++ Spring1ejer3part1.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <?php
+        $num = 2;
+        $double = 9.5;
+        $string = "hola soy jonathan";
+        $boo = true;
+
+        echo $num ," , ", $double , " , ", $string, " , ", $boo; 
+        echo " , "; 
+        define("yo", "Jonathan");
+        echo "mi nombre es " .  yo;
+        ?>
+    </head>
+</html>
